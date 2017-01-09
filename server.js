43e09162434f81b6027fae6c3d3267a5f01cbff5
Tri Le <trile@users.noreply.github.com>
2017-01-09 11:33:36 +0700

    Adding user model schema

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-var mongoose = require('mongoose');
+
 var app = express();
 
-var User = require('./models/user');
 
 
 var MONGODB_URI = 'mongodb://localhost:27017/ecommerce';
@@ -17,21 +16,6 @@ mongoose.connect(MONGODB_URI, function(err) {
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extend: true}));
-
-app.post('/create-user', function(req, res, next) {
-    var user = new User();
-
-    user.profile.name = req.body.name;
-    user.password = req.body.password;
-    user.email = req.body.email;
-
-    user.save(function(err) {
-        if (err) return next(err);
-        res.json('Successfully created a new user');
-    })
-});
-
-
 
 app.listen(3000, function(err) {
      if (err) throw err;
