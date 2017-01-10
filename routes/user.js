aa64809aa5969cc47ef8e51bc319ec96f9d0b4ae
Tri Le <trile@users.noreply.github.com>
2017-01-10 16:27:15 +0700

    Add sign up page with login form

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,12 @@
 var router = require('express').Router();
 var User = require('../models/user');
 
+router.get('/signup', function(req, res, next) {
+    res.render('accounts/signup', {
+        errors: req.flash('errors')
+    });
+})
+
 router.post('/signup', function(req, res) {
     var user = new User();
     user.profile.name = req.body.name;
@@ -9,15 +15,18 @@ router.post('/signup', function(req, res) {
 
     User.findOne({email: req.body.email}, function(err, existingUser) {
         if (existingUser) {
-            console.log(req.body.email + " is already exist");
+            // console.log(req.body.email + " is already exist");
+            req.flash('errors', 'Account with that email address already exists');
             return res.redirect('/signup');
         } else {
             user.save(function(err, user) {
                 if (err) return next(err);
             })
-            res.json("New user has been ceated");
+
+            return res.redirect('/');
+            // res.json("New user has been ceated");
         }
-    })
-})
+    });
+});
 
 module.exports = router;
